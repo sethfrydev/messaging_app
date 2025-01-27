4b757353a96375b341ab85c73d02f9e96b4e84ed
sethfrydev <solutions@sethfry.dev>
2025-01-27 07:29:38 -0500

    fix cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Allow requests from any origin for development (use specific domains in production)
+    origins Rails.env.production? ? 'https://seth-messaging-app-2f5c57a970bc.herokuapp.com' : '*' # Allow all in dev, restrict in production
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             credentials: Rails.env.production? # Only allow credentials in production
   end
 end
