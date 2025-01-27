51b157e16f5e96c5900597f8f6ecdaf79faa1923
sethfrydev <solutions@sethfry.dev>
2025-01-27 07:35:59 -0500

    fix cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://seth-messaging-app-2f5c57a970bc.herokuapp.com' : '*' # Allow all in dev, restrict in production
+    origins Rails.env.production? ? 'https://messaging-web.netlify.app' : '*' # Allow all in dev, restrict in production
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
